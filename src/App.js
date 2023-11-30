1458403a3a0ed17de8800d1c93e047bf8648f0c2
PhamVanTinh273 <phamvantinh27032004@gmail.com>
2023-11-30 22:47:30 +0700

    democuoiki

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Main page/Home';
+import Slide from './Main page/Slide';
+
+function App() {
+  return (<div>
+    <Home/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Main page/Slide.js
@@ -0,0 +1,6 @@
+function Slide() {
+    return(
+        <div></div>
+    )
+}
+export default Slide
